b3b4204e162985b92961b90a0bc0008d6bcb60b3
cjn <cjn@>
2025-06-01 19:25:36 +0800

    [feat]:implemet configuration module and init test

---
+++ config.py
@@ -0,0 +1,12 @@
+
+# 文件路径
+CALIBRATION_IMAGE_DIR = "data/calibration_images/" # 标定用图片路径
+TEST_IMAGE_DIR = "data/test_images/"
+OUTPUT_DIR = "output/"
+CAMERA_PARAMS_PATH = "output/stereo_params.yml"
+
+
+# 标定相关参数
+Image_Number = 13
+CHESSBOARD_SIZE = (9, 6)  # (内角点数量 a, 内角点数量 b)
+SQUARE_SIZE_MM = 25       # 棋盘格尺寸（毫米）
---
+++ main.py
@@ -0,0 +1,10 @@
+import config
+
+
+def main():
+    print("Project setup is working!")
+    print(f"Loading calibration images from: {config.CALIBRATION_IMAGE_DIR}")
+
+
+if __name__ == "__main__":
+    main()
