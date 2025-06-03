a429d802317ce984b7aa893a34b53b28fd16d029
cjn <cjn@>
2025-06-04 00:05:51 +0800

    feat(config): add criteria for mono and stereo calibration

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 # 文件路径
 CALIBRATION_IMAGE_DIR = "data/calibration_images/" # 标定用图片路径
@@ -14,3 +15,6 @@ SQUARE_SIZE_MM = 25        # 棋盘格尺寸（毫米）
 IMAGE_SIZE = (640, 480)    # 图片分辨率
 
 VISUALIZE_STEPS = False
+
+MONO_CALIB_CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+STEREO_CALIB_CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-5)
